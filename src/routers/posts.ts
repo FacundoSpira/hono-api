e2b2a9b446b87ce8addb41c3c080b8daff82dc31
Facundo Spira <facundospira@gmail.com>
2025-09-21 20:52:08 -0300

    chore: add a few more methods

--- src/routers/posts.ts
+++ src/routers/posts.ts
@@ -1,23 +1,46 @@
 import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { db } from "@/database/db";
 import { postsTable } from "@/database/schema";
 import { createRouter } from "@/lib/create-app";
 
-const posts = createRouter();
+const postsRouter = createRouter();
 
 const createPostSchema = z.object({
 	title: z.string().min(1),
 	content: z.string().min(1),
 });
 
-posts.get("/", async (c) => {
-	const posts = await db.select().from(postsTable).limit(10);
+postsRouter.get("/", async (c) => {
+	const limit = c.req.query("limit") ?? 10;
+	const offset = c.req.query("offset") ?? 0;
+
+	const posts = await db
+		.select()
+		.from(postsTable)
+		.limit(Number(limit))
+		.offset(Number(offset));
 
 	return c.json({ posts });
 });
 
-posts.post("/", zValidator("json", createPostSchema), async (c) => {
+postsRouter.get("/:id", async (c) => {
+	const id = c.req.param("id");
+
+	const [post] = await db
+		.select()
+		.from(postsTable)
+		.where(eq(postsTable.id, Number(id)));
+
+	if (!post) {
+		return c.json({ error: "Post not found" }, 404);
+	}
+
+	return c.json({ ...post });
+});
+
+postsRouter.post("/", zValidator("json", createPostSchema), async (c) => {
 	const { title, content } = c.req.valid("json");
 
 	const post = await db
@@ -28,4 +51,12 @@ posts.post("/", zValidator("json", createPostSchema), async (c) => {
 	return c.json({ post });
 });
 
-export default posts;
+postsRouter.delete("/:id", async (c) => {
+	const id = c.req.param("id");
+
+	await db.delete(postsTable).where(eq(postsTable.id, Number(id)));
+
+	return c.json({ message: "Post deleted" });
+});
+
+export default postsRouter;
